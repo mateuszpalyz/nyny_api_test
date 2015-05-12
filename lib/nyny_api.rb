4b62e6cc8ed0d23cbef3ff0a6afd7a2e579fcf3c
Mateusz Palyz <m.palyz@pilot.co>
2015-05-12 12:52:26 +0200

    add story and stories endpoints and test them

--- lib/nyny_api.rb
+++ lib/nyny_api.rb
@@ -2,10 +2,33 @@ require "nyny_api/version"
 
 module NynyApi
   require 'nyny'
+  require 'database'
+  require 'models/story'
 
   class HackerNews < NYNY::App
+
     get '/' do
       'Hello from hackernews'
+    end
+
+    get '/stories' do
+      Story.all.to_json
+    end
+
+    get '/stories/:id' do
+      Story.find(params[:id]).to_json
+    end
+
+    post 'stories' do
+
+    end
+
+    patch '/stories/:id' do
+
+    end
+
+    get '/stories/:id/url' do
+
     end
   end
 end
--- test/test_nyny_api.rb
+++ test/test_nyny_api.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'rack/test'
 require 'rack/lint'
 require 'nyny_api'
+require  'database_cleaner'
 
 class TestNynyApi < Minitest::Test
   include Rack::Test::Methods
@@ -11,10 +12,38 @@ class TestNynyApi < Minitest::Test
     Rack::Lint.new(HackerNews.new)
   end
 
+  def setup
+    DatabaseCleaner.start
+    Story.create!(title: 'Lorem ipsum', url: 'http://www.lipsum.com/')
+  end
+
   def test_it_does_something_useful
     get '/'
 
     assert last_response.ok?
     assert_equal 'Hello from hackernews', last_response.body
+  end
+
+  def test_getting_list_of_all_submitted_stories
+    Story.create!(title: 'Ipsum lorem', url: 'http://www.ipsum.com/')
+
+    get '/stories'
+    response = JSON.parse last_response.body
+
+    assert last_response.ok?
+    assert_equal 'Lorem ipsum', response.first['title']
+    assert_equal 'Ipsum lorem', response.last['title']
+  end
+
+  def test_getting_a_single_story
+    get '/stories/1'
+    response = JSON.parse last_response.body
+
+    assert last_response.ok?
+    assert_equal 'Lorem ipsum', response['title']
+  end
+
+  def teardown
+    DatabaseCleaner.clean
   end
 end
